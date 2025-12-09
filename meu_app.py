d77f3de977cdb8da3a2fd014f53d4931c00019c1
Carlos-Alipio <carlosalipioflores@gmail.com>
2025-12-09 18:15:51 -0300

    .

--- meu_app.py
+++ meu_app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 
 st.set_page_config(
-    page_title="Dashboard Pro",
+    page_title="CGNA - GOL",
     page_icon="🚀",  # Pode ser um emoji ou o caminho de uma imagem 'logo.png'
     layout="wide"    # "wide" usa a tela toda, "centered" centraliza o conteúdo
 )
