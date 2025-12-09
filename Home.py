17daf2ae0fa1d16755d65fbd6fba17ae803dcf6f
Carlos-Alipio <carlosalipioflores@gmail.com>
2025-12-09 18:24:28 -0300

    .

--- Home.py
+++ Home.py
@@ -5,3 +5,5 @@ st.set_page_config(
     page_icon="🚀",  # Pode ser um emoji ou o caminho de uma imagem 'logo.png'
     layout="wide"    # "wide" usa a tela toda, "centered" centraliza o conteúdo
 )
+
+st.write("Olá.")
