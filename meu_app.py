29ab1c504177c7deabc222d4f8d4321d03481226
Carlos-Alipio <carlosalipioflores@gmail.com>
2025-12-09 18:15:03 -0300

    .

--- meu_app.py
+++ meu_app.py
@@ -1,1 +1,7 @@
-print('teste')
+import streamlit as st 
+
+st.set_page_config(
+    page_title="Dashboard Pro",
+    page_icon="🚀",  # Pode ser um emoji ou o caminho de uma imagem 'logo.png'
+    layout="wide"    # "wide" usa a tela toda, "centered" centraliza o conteúdo
+)
