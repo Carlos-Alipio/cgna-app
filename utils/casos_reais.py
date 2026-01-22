b83837553ff1133b3b03523c799b87359285cfb6
Carlos-Alipio <carlosalipioflores@gmail.com>
2026-01-22 14:42:35 -0300

    .

--- utils/casos_reais.py
+++ utils/casos_reais.py
@@ -184,13 +184,14 @@ CASOS_BLINDADOS = [
     },
     {
         "id": "CASO_16",
-        # ... mantenha o texto original ...
+        "desc": "Sintaxe com Barra (DEC 01/02 e MON/TUE) + Herança de Dias",
+        "b": "2512012133",
+        "c": "2602281630",
+        "d": "DEC 01/02 2133-0115 DEC 02 TIL FEB 28 MON TUE THU 0745-1630 WED FRI 1020-1600 SAT 0745-1630 DEC 02 TIL JAN 30 MON/TUE TIL THU/FRI 1940-0115 FRI/SAT 2040-0115",
         "esperado": {
-            "qtd_slots": 130, # <--- ATUALIZADO (Correção do bug de leitura WED/THU)
-            # Verifique se as datas de início/fim mudaram. 
-            # Se mudaram, rode no Laboratório para pegar os novos valores exatos.
-            "primeiro_inicio": "...", 
-            "ultimo_fim": "..."
+            "qtd_slots": 130,
+            "primeiro_inicio": "01/12/2025 21:33",
+            "ultimo_fim": "28/02/2026 16:30"  # <--- CORRIGIDO AQUI (Era 07:45)
         }
     },
     {
