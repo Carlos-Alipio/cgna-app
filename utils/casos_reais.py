f9436d2d0967445fb5514a607181680755e8235a
Carlos-Alipio <carlosalipioflores@gmail.com>
2026-01-22 18:06:48 -0300

    .

--- utils/casos_reais.py
+++ utils/casos_reais.py
@@ -727,4 +727,16 @@ CASOS_BLINDADOS = [
             "ultimo_fim": "28/02/2026 16:30"
         }
     },
+    {
+        "id": "CASO_61",
+        "desc": "Listas Discretas: Sequências longas de dias específicos com horários alternados",
+        "b": "2601051000",
+        "c": "2601301550",
+        "d": "JAN 05 08 10 12 15 17 19 22 24 26 29 1000-1620 JAN 06 07 09 13 14 16 20 21 23 27 28 30 1000-1550",
+        "esperado": {
+            "qtd_slots": 23,
+            "primeiro_inicio": "05/01/2026 10:00",
+            "ultimo_fim": "30/01/2026 15:50"
+        }
+    },
 ]
