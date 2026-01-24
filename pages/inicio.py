9f249e0724b7e6523cd7febd5dc5fc632989618b
Carlos-Alipio <carlosalipioflores@gmail.com>
2026-01-24 15:52:37 -0300

    .

--- pages/inicio.py
+++ pages/inicio.py
@@ -51,12 +51,4 @@ st.success("Acesso autorizado ao painel operacional.")
 
 st.divider()
 
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.metric("Obras em Monitoramento", "14")
-with col2:
-    st.metric("NOTAMs Ativos", "22", delta="-3")
-with col3:
-    st.metric("Tempo Médio de Cadastro", "4m", delta="15s", delta_color="inverse")
 
-st.info("Utilize o menu à esquerda para gerenciar os agendamentos das obras.")
